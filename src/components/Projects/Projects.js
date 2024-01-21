98044419eab95e60ce915436b0e25a1f3954c3f6
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-01-21 14:30:17 +0530

    added projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
+// import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
@@ -25,9 +25,9 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="#"
+              title="Chit - Chat - Champs"
+              description="Chit Chat Champs is a real-time chat application developed with the MERN stack and powered by Socket.io. Enjoy instant messaging, private conversations, and group chats. With features like user authentication, notifications, and customizable profiles, it offers a dynamic and secure platform for seamless communication."
+              ghLink="https://github.com/lokesh-7977/Chit-Chat-Champs"
               demoLink="#"
             />
           </Col>
@@ -36,9 +36,9 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="#"
+              title="Swip Tory"
+              description="Swiptory, an immersive web story platform, caters to readers and writers with captivating narratives. Express appreciation through likes, bookmark favorites, and explore diverse categories. With user authentication and personalized feeds, it provides an engaging space for dynamic storytelling."
+              ghLink="https://github.com/lokesh-7977/Swip-Tory"
               demoLink="#"
             />
           </Col>
@@ -47,20 +47,20 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="#"
-              demoLink="#"              
+              title="Pocket Notes App"
+              description="Pocket Notes,a React-powered app utilizing the Context API, provides a seamless note-taking experience. With an intuitive interface and efficient state management, organize and retrieve your thoughts effortlessly. Secure authentication and collaborative features enhance productivity for users across various devices."
+              ghLink="https://github.com/lokesh-7977/Pocket-Notes-App"
+              demoLink="notes-pocket.netlify.app"
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
+              title="Super App"
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="#"
+              ghLink="www.google.com"
               demoLink="#"
             />
           </Col>
@@ -69,9 +69,9 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
+              title="Todo List"
               description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="#"
+              ghLink="www.google.com"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
@@ -80,13 +80,14 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
+              title="Rock Paper Scissors"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
               ghLink="#"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
+            />{" "}
+            
+          </Col> */}
         </Row>
       </Container>
     </Container>
